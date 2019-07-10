0b42561cfafb599ec7e109eed587ddd1a5282861
Balazs Konig <bkonig@jaguarlandrover.com>
2019-07-10 18:06:39 +0100

    Created board using dictionary of Piece class instances

--- create_board.py
+++ create_board.py
@@ -10,5 +10,37 @@ class Board():
 
 def initial_board():
     # TODO: populate initial board with 64 pieces
-    initial_board = None
+    pieces = {}
+    for i in range(1,9):
+        for j in range(1,9):
+            pieces[(i,j)] = Piece(None, None)
+    # Colours
+    for piece in pieces:
+        if piece[0] > 2 and piece[0] < 7:
+            continue
+        elif piece[0] < 3:
+            pieces[piece].colour = "W"
+        else:
+            pieces[piece].colour = "B"
+    # Types
+    for piece in pieces:
+        if piece[0] > 2 and piece[0] < 7:
+            continue
+        elif piece[0] == 2 or piece[0] == 7:
+            pieces[piece].type = "Pawn"
+        elif piece[1] == 1 or piece[1] == 8:
+            pieces[piece].type = "Rook"
+        elif piece[1] == 2 or piece[1] == 7:
+            pieces[piece].type = "Knight"
+        elif piece[1] == 3 or piece[1] == 6:
+            pieces[piece].type = "Bishop"
+        elif piece[1] == 4:
+            pieces[piece].type = "Queen"
+        elif piece[1] == 5:
+            pieces[piece].type = "King"
+    # Create board
+    castle = [True, True, True, True]
+    initial_board = Board(pieces, castle)
     return initial_board
+
+initial_board()
